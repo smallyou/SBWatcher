fc1590a53325aeaf24ae145ee5cebf2d9db5d04b
23 <a23@23deMacBook-Pro.local>
2017-07-22 12:07:13 +0800

    初始化结构

---
+++ SBWatcher/SBWatcher/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SBWatcher
+//
+//  Created by 23 on 2017/7/22.
+//  Copyright © 2017年 smallyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
